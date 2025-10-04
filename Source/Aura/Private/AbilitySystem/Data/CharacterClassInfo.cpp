cfb82a095f5d91db5a8ce0dceb9656ac3464a13c
talal-jpg <talal.qazi94@live.com>
2025-10-04 17:48:13 +0800

    deleted afterRedo

--- Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "AbilitySystem/Data/CharacterClassInfo.h"
-
-FCharacterClassDefaultInfo UCharacterClassInfo::GetCharacterClassDefaultInfo(ECharacterClass CharacterClass)
-{
-	return CharClassToCharClassInfo.FindChecked(CharacterClass);
-}
